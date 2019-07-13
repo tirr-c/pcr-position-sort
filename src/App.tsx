59418268902431e073236723b5ffde128f8377f8
Wonwoo Choi <chwo9843@gmail.com>
2019-07-14 00:33:11 +0900

    Add example parties

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import React from 'react';
 import { arrayMove } from 'react-sortable-hoc';
 
 import Board from './Board';
-import { sampleUnits, unitData } from './unit-data';
+import { getRandomExample, sampleUnits, unitData } from './unit-data';
 
 const BoardWrapper = styled('div')`
     display: flex;
+    align-items: center;
     justify-content: center;
 `;
 
@@ -17,11 +18,13 @@ const styles = css`
 `;
 
 function App() {
-    const [units, setUnits] = React.useState(() => sampleUnits(5));
+    const [units, setUnits] = React.useState(getRandomExample);
     const handleSortEnd = React.useCallback(({ oldIndex, newIndex }) => setUnits(units => arrayMove(units, oldIndex, newIndex)), []);
     return (
         <BoardWrapper>
+            <span>뒤</span>
             <Board className={styles.board} units={units} axis="x" onSortEnd={handleSortEnd} />
+            <span>앞</span>
         </BoardWrapper>
     );
 }
--- src/unit-data.ts
+++ src/unit-data.ts
@@ -9,6 +9,10 @@ function random(maxExclusive: number): number {
     return base % maxExclusive;
 }
 
+function shuffle<T>(list: T[]): T[] {
+    return sample(list, list.length);
+}
+
 function sample<T>(list: T[], n: number): T[] {
     const shuffleList = [...list];
     const len = list.length;
@@ -30,7 +34,24 @@ export interface Unit {
 }
 
 export const unitData: Map<string, Unit> = new Map(rawUnitData.map(unit => [unit.id, unit]));
+const sampleParties = [
+    ['100601', '100701', '102201', '103101', '104401'], // 악마왕국군
+    ['102101', '102301', '102501', '102801'], // 사렌디아 구호원
+    ['102601', '103301', '103801', '105201'], // 목장
+    ['103201', '103401', '104601', '104801'], // 메르쿠리우스 재단
+    // 아래는 너무 짧아서 제외
+    // ['105801', '105901', '106001'], // 미식전
+    // ['100101', '100201', '100301'], // 트윙클 위시
+    // ['100401', '102001', '103601'], // 리틀 리리컬
+    // ['101201', '101501', '104001'], // 포레스티에
+    // ['101601', '101801', '105001'], // 루센트 학원
+];
 
 export function sampleUnits(n: number): string[] {
     return sample(rawUnitData, n).map(({ id }) => id);
+}
+
+export function getRandomExample(): string[] {
+    const idx = random(sampleParties.length);
+    return shuffle(sampleParties[idx]);
 }
