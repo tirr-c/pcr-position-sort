1ad256be5176605d1591a94dad70745c031cf235
Wonwoo Choi <chwo9843@gmail.com>
2019-07-14 00:40:20 +0900

    Add description

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,30 @@ import { arrayMove } from 'react-sortable-hoc';
 import Board from './Board';
 import { getRandomExample, sampleUnits, unitData } from './unit-data';
 
+const BodyWrapper = styled('div')`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > * {
+        flex: 0;
+    }
+`;
+
+const Spacer = styled('div')`
+    flex: 1;
+`;
+
 const BoardWrapper = styled('div')`
+    width: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
+
+    > * + * {
+        margin-left: 8px;
+    }
 `;
 
 const styles = css`
@@ -21,11 +41,21 @@ function App() {
     const [units, setUnits] = React.useState(getRandomExample);
     const handleSortEnd = React.useCallback(({ oldIndex, newIndex }) => setUnits(units => arrayMove(units, oldIndex, newIndex)), []);
     return (
-        <BoardWrapper>
-            <span>뒤</span>
-            <Board className={styles.board} units={units} axis="x" onSortEnd={handleSortEnd} />
-            <span>앞</span>
-        </BoardWrapper>
+        <BodyWrapper>
+            <h2>Party Sorted</h2>
+            <p>
+                드래그해서 파티 멤버를 순서대로 나열해 보세요.
+            </p>
+            <BoardWrapper>
+                <span>뒤</span>
+                <Board className={styles.board} units={units} axis="x" onSortEnd={handleSortEnd} />
+                <span>앞</span>
+            </BoardWrapper>
+            <Spacer />
+            <div>
+                <a href="https://github.com/tirr-c/pcr-position-sort">소스 코드 보기</a>
+            </div>
+        </BodyWrapper>
     );
 }
 
